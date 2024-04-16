3e7ba518a89f9da47d6aac188bd44526a9c1b7e4
Khaled Bouchama (V) <kbouchama@apple.com>
2024-04-16 14:56:35 -0700

    second commit

--- load_file_to_qgis.py
+++ load_file_to_qgis.py
@@ -1,6 +1,7 @@
 import os
 
-for folder_path, _, file_names in os.walk('/Users/khaledbouchama/Downloads/IND/Telangana'):
+dir_path = '<absolute path to dir with shapefiles>'
+for folder_path, _, file_names in os.walk(dir_path):
     for file_name in file_names:
         if file_name.lower().endswith('.shp'):
             # Construct the full path of the shapefile
